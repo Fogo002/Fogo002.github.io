021fedc70a0238d0515236468e1bdc24935e2a8d
André Ferreira <andreferreiragemeo@gmail.com>
2024-06-07 01:09:32 +0100

    links redes sociais

--- my-app/src/components/Footer/Footer.tsx
+++ my-app/src/components/Footer/Footer.tsx
@@ -1,24 +1,33 @@
-import './Footer.css'
-import facebook from '../../assets/facebook.png'
-import instagram from '../../assets/instagram.png'
-
-
-const Footer = () => {
-  
+import React from 'react';
+import './Footer.css';
+import facebook from '../../assets/facebook.png';
+import instagram from '../../assets/instagram.png';
+import youtube from '../../assets/youtube.png';
+const Footer: React.FC = () => {
     return (
-      <div className="Footer">
-        
-           
+        <div className="Footer">
             <div className='follow-us'>
                 <h5>Follow us</h5>
                 <ul>
-                    <li><img src={instagram} /></li>
-                    <li><img src={facebook} /></li>
+                    <li>
+                        <a href="https://www.instagram.com/todossomosespeciais23/" target="_blank" rel="noopener noreferrer">
+                            <img src={instagram} alt="Instagram" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.facebook.com/profile.php?id=100094503303826" target="_blank" rel="noopener noreferrer">
+                            <img src={facebook} alt="Facebook" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.youtube.com/@TodosSomosEspeciais" target="_blank" rel="noopener noreferrer">
+                            <img src={youtube} alt="Youtube" />
+                        </a>
+                    </li>
                 </ul>
-            
+            </div>
         </div>
-      </div>
-      
-    )
+    );
 }
-export default Footer
+
+export default Footer;
