3d7c9b2708e759c516b8281613d77832e041be91
André Ferreira <andreferreiragemeo@gmail.com>
2024-06-14 21:08:19 +0100

    dropdown

--- my-app/src/components/MenuDropdown/MenuDropdown.tsx
+++ my-app/src/components/MenuDropdown/MenuDropdown.tsx
@@ -1,10 +1,9 @@
 import * as React from "react";
 import { useState } from "react";
 import { Dropdown } from "react-bootstrap";
-import './MenuDropdown.css'; // Importa o arquivo CSS
 import { useNavigate } from "react-router-dom";
+import './MenuDropdown.css'; // Importa o arquivo CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 
 interface Item {
     nome: string;
@@ -21,40 +20,36 @@ const MenuDropdown: React.FC<MenuDropdownProps> = ({ itens, titulo }) => {
 
     const handleMouseEnter = () => setIsDropdownOpen(true);
     const handleMouseLeave = () => setIsDropdownOpen(false);
+
     const navigate = useNavigate();
 
     const handleMenuItemClick = (link: string) => {
         navigate(link);
+        setIsDropdownOpen(false); // Fecha o dropdown após a navegação
     };
-    
-    return (
-        <>
-            <Dropdown 
-                onMouseEnter={handleMouseEnter} 
-                onMouseLeave={handleMouseLeave} 
-                show={isDropdownOpen}
-            >
-                <Dropdown.Toggle 
-                    id="dropdown-basic" 
-                    className="custom-dropdown-toggle"
-                >
-                    {titulo}
-                </Dropdown.Toggle>
 
-                <Dropdown.Menu 
-                    onMouseEnter={handleMouseEnter} 
-                    onMouseLeave={handleMouseLeave} 
-                    show={isDropdownOpen}
-                >
-                    {itens.map((item: Item, index: number) => (
-                    <Dropdown.Item key={index} onClick={() => handleMenuItemClick(item.link)}>
+    return (
+        <Dropdown
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            show={isDropdownOpen}
+            style={{ height: "100%" }}
+        >
+            <Dropdown.Toggle id="dropdown-basic" className="custom-dropdown-toggle">
+                {titulo}
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu className={isDropdownOpen ? 'show' : ''}>
+                {itens.map((item: Item, index: number) => (
+                    <Dropdown.Item
+                        key={index}
+                        onClick={() => handleMenuItemClick(item.link)}
+                    >
                         {item.nome}
                     </Dropdown.Item>
                 ))}
-
-                </Dropdown.Menu>
-            </Dropdown>
-        </>
+            </Dropdown.Menu>
+        </Dropdown>
     );
 };
 
