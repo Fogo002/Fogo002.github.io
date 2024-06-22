67a69f95a8e6f4add088dd0c246171337d1939c7
André Ferreira <andreferreiragemeo@gmail.com>
2024-06-22 19:03:15 +0100

    fix de algumas coias

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -11,6 +11,8 @@ import FAQ from './pages/Mascote/Mascote';
 import Revista from './pages/Revista/Revista';
 import Mascote from './pages/Mascote/Mascote';
 import Extras from './pages/Extras/Extras';
+import History from './pages/History/History';
+
 
 function App() {
   return (
@@ -27,6 +29,7 @@ function App() {
             <Route path='/mascote' element={<Mascote />} />
             <Route path='/revista' element={<Revista />} />
             <Route path='/extras' element={<Extras />} />
+            <Route path='/historia' element={<History />} />
             <Route path='*' element={<Navigate to="/" replace />} />
           </Routes>
         </div>
